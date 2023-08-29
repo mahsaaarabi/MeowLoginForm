35390b084baac4d0cd5c4bdb77ed569a1aec3535
mahsaaarabi <mahsaaarabi950@gmail.com>
2023-08-30 00:22:32 +0330

    v-alpha 1

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-document.getElementById("myform")[2].addEventListener("click", function(e) {
+window.addEventListener("DOMContentLoaded", function () {
+    if (navigator.storage) {
+        if (localStorage.length > 0) {
+            window.location = "account.html";
+        }
+    } else {
+        return false;
+    }
+});
+
+document.getElementById("myform")[2].addEventListener("click", function (e) {
     e.preventDefault();
     if (document.getElementById('myform')[1].value.length > 7 && document.getElementById('myform')[0].value == "") {
         document.getElementById('myform')[0].style.border = "2px solid red";
@@ -9,9 +19,13 @@ document.getElementById("myform")[2].addEventListener("click", function(e) {
             if (document.getElementById('myform')[1].value.length > 7) {
 
                 // If evething was good enough, it's working...
-                console.log(`Your username is ${document.getElementById('myform')[0].value} and your password is ${document.getElementById('myform')[1].value}`);
-                document.getElementById('myform')[1].style.border = "0px solid transparent";
-                document.getElementById("myform").submit();
+                if (navigator.storage) {
+                    localStorage.setItem(`${document.getElementById('myform')[0].value}`, `${document.getElementById('myform')[1].value}`);
+                    document.getElementById('myform')[1].style.border = "0px solid transparent";
+                    alert('Congratulations! You have successfully');
+                } else {
+                    alert('There was a problem!');
+                }
             } else {
                 document.getElementById('myform')[1].style.border = "2px solid red";
             }
